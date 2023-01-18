becfdb571122cfc232d07e5cf2e61df6d470f73e
SelfDev <51464152+RoxDevvv@users.noreply.github.com>
2023-01-18 22:09:12 +0100

    Update test_Chat.user.js

--- test_Chat.user.js
+++ test_Chat.user.js
@@ -1,1 +1,16 @@
-document.body.style.backgroundColor = "red";
+// ==UserScript==
+// @name         test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.style.backgroundColor = "red";
+})();
+
